106e06b6859c987fb9fe6d728d39d20425cdae49
Dara Ouk <vdb.dara.ouk@gmail.com>
2018-01-22 22:08:45 +0700

    added helper and task to send emails via GMail, moved SharedPreferencesHelper to utils dir

---
+++ app/src/main/java/com/douk/madfeedbacktool/utils/SendEmailHelper.java
@@ -0,0 +1,77 @@
+package com.douk.madfeedbacktool.utils;
+
+import android.util.Log;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendEmailHelper {
+
+    final String emailPort = "587";// gmail's smtp port
+    final String smtpAuth = "true";
+    final String starttls = "true";
+    final String emailHost = "smtp.gmail.com";
+
+    String fromEmail = "feedback.madcambodia@gmail.com";
+    String fromPassword = "Master@mad1";
+    List<String> toEmailList = new ArrayList<>();
+    String emailSubject;
+    String emailBody;
+
+    Properties emailProperties;
+    Session mailSession;
+    MimeMessage emailMessage;
+
+    public SendEmailHelper(String emailSubject, String emailBody) {
+        this.emailSubject = emailSubject;
+        this.emailBody = emailBody;
+        toEmailList.add("ouk.dara@outlook.com");
+        toEmailList.add("vdb.dara.ouk@gmail.com");
+
+        emailProperties = System.getProperties();
+        emailProperties.put("mail.smtp.port", emailPort);
+        emailProperties.put("mail.smtp.auth", smtpAuth);
+        emailProperties.put("mail.smtp.starttls.enable", starttls);
+        Log.i("GMail", "Mail server properties set.");
+    }
+
+    public MimeMessage createEmailMessage() throws AddressException,
+            MessagingException, UnsupportedEncodingException {
+
+        mailSession = Session.getDefaultInstance(emailProperties, null);
+        emailMessage = new MimeMessage(mailSession);
+
+        emailMessage.setFrom(new InternetAddress(fromEmail, fromEmail));
+        for (String toEmail : toEmailList) {
+            Log.i("GMail","toEmail: "+toEmail);
+            emailMessage.addRecipient(Message.RecipientType.TO,
+                    new InternetAddress(toEmail));
+        }
+
+        emailMessage.setSubject(emailSubject);
+        emailMessage.setContent(emailBody, "text/html");// for a html email
+        // emailMessage.setText(emailBody);// for a text email
+        Log.i("GMail", "Email Message created.");
+        return emailMessage;
+    }
+
+    public void sendEmail() throws AddressException, MessagingException {
+
+        Transport transport = mailSession.getTransport("smtp");
+        transport.connect(emailHost, fromEmail, fromPassword);
+        Log.i("GMail","allrecipients: "+emailMessage.getAllRecipients());
+        transport.sendMessage(emailMessage, emailMessage.getAllRecipients());
+        transport.close();
+        Log.i("GMail", "Email sent successfully.");
+    }
+}
---
+++ app/src/main/java/com/douk/madfeedbacktool/utils/SharedPreferencesHelper.java
@@ -0,0 +1,24 @@
+package com.douk.madfeedbacktool.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by douk on 1/22/18.
+ */
+
+public class SharedPreferencesHelper {
+    private static final String PREFERENCES_FILE = "madfeedbacktool_settings";
+
+    public static String readSharedSetting(Context ctx, String settingName, String defaultValue) {
+        SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        return sharedPref.getString(settingName, defaultValue);
+    }
+
+    public static void saveSharedSetting(Context ctx, String settingName, String settingValue) {
+        SharedPreferences sharedPref = ctx.getSharedPreferences(PREFERENCES_FILE, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString(settingName, settingValue);
+        editor.apply();
+    }
+}
